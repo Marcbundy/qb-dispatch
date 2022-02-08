9dbb7d05fe017d7aa6645faf9edbc14918bbaaa2
Joe Szymkowicz <joe.szymkowicz@gmail.com>
2022-02-08 14:55:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dispatch System
+
+Dispatch system that works with our mdt.
+
+Available alerts:
+
+[+] un-dispatch:bankrobbery - Bank Robbery
+
+[+] un-dispatch:storerobbery - Store Robbery
+
+[+] un-dispatch:houserobbery - House Robbery
+
+[+] un-dispatch:jewelrobbery - Vangelico Robbery
+
+[+] un-dispatch:jailbreak - Jail Break
+
+[+] un-dispatch:carjacking - Vehicle Theft
+
+[+] un-dispatch:gunshot - Shots Fired
+
+
+# Credit
+By FlawwsX - https://github.com/FlawwsX/erp_dispatch
---
+++ sh_config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config["AuthorizedJobs"] = {
+    "police"
+}
+
+Config["AutoAlerts"] = {
+    ["CarJacking"] = true,
+    ["GunshotAlert"] = true
+}
+
+Config["CoreSettings"] = {
+    ["Core"] = "qbcore", -- Your core | qbcore, esx
+    ["ESX"] = {
+        ["ESXTrigger"] = "esx:getSharedObject"
+    },
+    ["QBCore"] = {
+        ["QBCoreVersion"] = "new", -- new = using exports | old = using events
+        ["QBCoreExport"] = exports['qb-core']:GetCoreObject(), -- If you are using old qbcore version just remove this line
+        ["QBUSTrigger"] = "QBCore:GetObject"
+    }
+}
