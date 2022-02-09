fdec669b8e68551df3a8c2972c2526c420fdb5e2
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-02-08 23:15:50 -0500

    Removed ESX Version & NP
    ESX Version & NP  and changed from un- to qb-

--- sv_main.lua
+++ sv_main.lua
@@ -1,7 +1,5 @@
 local QBCore = nil
 
-local ESX = nil
-
 local calls = 0
 
 function isAuth(job)
@@ -23,199 +21,65 @@ LoadQBCoreVersion = function()
             end
         end 
     end)
-    RegisterServerEvent("un-dispatch:bankrobbery", function(coords)
+    RegisterServerEvent("qb-dispatch:bankrobbery", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "bankrobbery", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "bankrobbery", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:storerobbery", function(coords)
+    RegisterServerEvent("qb-dispatch:storerobbery", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "storerobbery", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "storerobbery", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:houserobbery", function(coords)
+    RegisterServerEvent("qb-dispatch:houserobbery", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "houserobbery", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "houserobbery", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:jewelrobbery", function(coords)
+    RegisterServerEvent("qb-dispatch:jewelrobbery", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "jewelrobbery", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "jewelrobbery", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:jailbreak", function(coords)
+    RegisterServerEvent("qb-dispatch:jailbreak", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "jailbreak", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "jailbreak", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:carjacking", function(coords)
+    RegisterServerEvent("qb-dispatch:carjacking", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "carjacking", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "carjacking", coords)
             end
         end
     end)
-    RegisterServerEvent("un-dispatch:gunshot", function(coords)
+    RegisterServerEvent("qb-dispatch:gunshot", function(coords)
         for idx, id in pairs(QBCore.Functions.GetPlayers()) do
             local xPlayer = QBCore.Functions.GetPlayer(id)
             if isAuth(xPlayer.PlayerData.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.PlayerData.source, "gunshot", coords)
+                TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "gunshot", coords)
             end
         end
     end)
 end
 
-LoadESXVersion = function()
-    RegisterServerEvent("dispatch:svNotify", function(data)
-        calls = calls + 1
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent('dispatch:clNotify', xPlayer.source, data, calls)
-            end
-        end 
-    end)
-    RegisterServerEvent("un-dispatch:bankrobbery", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "bankrobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:storerobbery", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "storerobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:houserobbery", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "houserobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:jewelrobbery", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "jewelrobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:jailbreak", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "jailbreak", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:carjacking", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "carjacking", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:gunshot", function(coords)
-        for idx, id in pairs(ESX.GetPlayers()) do
-            local xPlayer = ESX.GetPlayerFromId(id)
-            if isAuth(xPlayer.job.name) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "gunshot", coords)
-            end
-        end
-    end)
-end
 
-LoadNopixelVersion = function()
-    RegisterServerEvent("dispatch:svNotify", function(data)
-        calls = calls + 1
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent('dispatch:clNotify', xPlayer.source, data, calls)
-            end
-        end 
-    end)
-    RegisterServerEvent("un-dispatch:bankrobbery", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "bankrobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:storerobbery", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "storerobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:houserobbery", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "houserobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:jewelrobbery", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "jewelrobbery", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:jailbreak", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "jailbreak", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:carjacking", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "carjacking", coords)
-            end
-        end
-    end)
-    RegisterServerEvent("un-dispatch:gunshot", function(coords)
-        for idx, id in pairs(GetPlayers()) do
-            local xPlayer = exports[Config["CoreSettings"]["NPBase"]["CoreName"]]:getModule("Player"):GetUser(tonumber(id))
-            if isAuth(xPlayer.job) then
-                TriggerClientEvent("un-dispatch:createBlip", xPlayer.source, "gunshot", coords)
-            end
-        end
-    end)
-end
 
 Citizen.CreateThread(function()
     if Config["CoreSettings"]["Core"] == "qbcore" then
@@ -225,10 +89,5 @@ Citizen.CreateThread(function()
             TriggerEvent(Config["CoreSettings"]["QBCore"]["QBUSTrigger"], function(obj) QBCore = obj end)
         end
         LoadQBCoreVersion()
-    elseif Config["CoreSettings"]["Core"] == "esx" then
-        TriggerEvent(Config["CoreSettings"]["ESX"]["ESXTrigger"], function(obj) ESX = obj end)
-        LoadESXVersion()
-    elseif Config["CoreSettings"]["Core"] == "npbase" then
-        LoadNopixelVersion()
     end
 end)
