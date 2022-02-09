49607bdf0e64791bd7d2befcdd22979fd3a20ac8
trclassic92 <82931913+trclassic92@users.noreply.github.com>
2022-02-09 16:00:25 -0500

    Casino Alert (Updated PR)

--- sv_main.lua
+++ sv_main.lua
@@ -80,7 +80,15 @@ RegisterServerEvent("qb-dispatch:officerdown", function(coords)
 for idx, id in pairs(QBCore.Functions.GetPlayers()) do
         local xPlayer = QBCore.Functions.GetPlayer(id)
         if isAuth(xPlayer.PlayerData.job.name) then
-            TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "10-13A Officer Down", coords)
+            TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "10-99 Officer Down", coords)
+        end
+    end
+end)
+RegisterServerEvent("qb-dispatch:casinorobbery", function(coords)
+    for idx, id in pairs(QBCore.Functions.GetPlayers()) do
+        local xPlayer = QBCore.Functions.GetPlayer(id)
+        if isAuth(xPlayer.PlayerData.job.name) then
+            TriggerClientEvent("qb-dispatch:createBlip", xPlayer.PlayerData.source, "casinorobbery", coords)
         end
     end
 end)
